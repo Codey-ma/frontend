aa687c2a8bc6c71a8466233568bc2cf89558748e
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-08-25 13:54:21 +0530

    Main Layout and Header and Footer

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import QandA from '../QandA/QandA'
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      {/* <QandA /> */}
+    </div>
+  )
+}
+
+export default Home
