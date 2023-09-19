8162a4ebc990392798cde78ae20981c4a09dad87
Akram-2002 <Akrambusiness6@gmail.com>
2023-09-19 10:59:04 +0100

    add files

--- codey-site/src/App.js
+++ codey-site/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignIn from './components/Signin';
 import SignUp from './components/Signup';
 
@@ -11,6 +11,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         {/* Add more routes as needed */}
+        <Route path="/" element={<Navigate to="/signin" />} />
       </Routes>
     </Router>
     
