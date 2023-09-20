48ebf913fbdefb82b0fc98adc9e17c19ed3da32f
Akram-2002 <Akrambusiness6@gmail.com>
2023-09-20 12:41:00 +0100

    add files

--- codey-site/src/components/Signin/index.js
+++ codey-site/src/components/Signin/index.js
@@ -15,96 +15,92 @@ import Google from  '../../assets/google.png';
 const Signin = () => {
   return (
 <div
-      className="overflow-hidden bg-white flex flex-col justify-between gap-24 w-full pt-2 px-4"
+      className="overflow-hidden bg-white flex flex-row w-full pt-2 px-4"
       id="signinContainer"
     >
-      <div className="relative flex flex-col w-40 items-start">
-      <img
+      <div className="logo-container">
+        <img
           src={Logo}
-          className="relative w-16"
+          className="logo"
         />
         <img
           src={Codey}
-          className="w-24 h-8 absolute top-5 left-12"
+          className="logo logo-second"
           id="Codey"
         />
-        
       </div>
-      <div className="flex flex-row gap-20 items-end mx-40">
-        <div className="flex flex-row mt-32 gap-5 w-3/5 items-end">
+
+
+      <div className="flex flex-row gap-20 items-center mx-60" id="signin">
+        <div className="flex flex-row mt-60 gap-5 w-3/5 items-center">
           <div className="flex flex-col gap-12 w-1/2">
-            <div className="flex flex-col mr-16 gap-1 items-start">
-              <div
-                className="text-lg font-['Poppins'] font-bold"
-                id="SignInTo"
+            
+              <div className="text-8xl font-['Poppins'] font-bold"
+                id="Header"
               >
-                Sign in to{" "}
+                <h1>Sign in to {" "}</h1>
               </div>
               <div className="text-4xl font-['Poppins'] font-medium">
                 Your Account
               </div>
-            </div>
-            <div className="flex flex-col ml-1 gap-1 items-start">
+              <br />
+            
               <div className="font-['Poppins']">
                 If you don’t have an account register
-              </div>
               <div className="font-['Poppins']" id="YouCanRegisterHere1">
-                You can
-                {"   "}
-                <div
-                  className="font-semibold text-[#4d47c3] contents"
-                  id="YouCanRegisterHere"
-                >
-                 <Link to="/signup">Register here</Link>
-                </div>
+                You can{" "}
+                 <Link to="/signup" className="font-semibold custom-link" id="YouCanRegisterHere">Register here</Link>
+                
               </div>
             </div>
           </div>
-          <img
+        </div>
+        <div>
+        <img
             src={Saly}
-            className="mt-[108px]"
+            className="mt-[102px]"
             id="Saly"
           />
         </div>
-        <div className="flex flex-col gap-4 h-[543px] items-center">
+        <div className="flex flex-col gap-4 h-[543px] items-center" id="signin-form">
           <div className="text-3xl font-['Poppins'] font-medium mb-3">
             Sign in
           </div>
           <div className="bg-[rgba(31,_216,_145,_0.26)] flex flex-col justify-center pl-6 w-[369px] h-16 shrink-0 items-start mb-5 mr-px rounded-lg">
             <div className="font-['Poppins']">
-            <input type="text" class="Text-sm" placeholder="Enter email or user name" />
+            <input type="text" class="Text-sm input-field" placeholder="Enter email or user name" />
             </div>
           </div>
           <div className="bg-[rgba(31,_216,_145,_0.26)] flex flex-row justify-between w-[369px] h-16 shrink-0 items-center px-8 rounded-lg">
-          <input type="password" class="Text-sm" placeholder="Password" />
-            <img
-              src={Invisible}
-              className="w-4 shrink-0"
-              id="Invisible"
-            />
+          <input type="password" class="Text-sm input-field" placeholder="Password" />
+            
           </div>
           <div className="text-xs font-['Poppins'] text-[#b0b0b0] self-end mb-8">
             Forgot password ?
           </div>
-            <div className="font-['Poppins'] font-medium text-white"><button className='bg-green-500 text-white px-4 py-2 rounded-lg'>Login</button></div>
+            <div className="font-['Poppins'] font-medium text-white">
+              <button className='bg-green-500 text-white px-4 py-2 rounded-lg'>
+                Login
+                </button>
+            </div>
           
           <div className="font-['Poppins'] font-medium text-[#b5b5b5] mb-6">
             or continue with
           </div>
-          <div className="flex flex-row gap-5 w-40 items-start">
+          <div className="flex flex-row gap-5 w-40 items-center" id='social-icons'>
             <img
               src={Facebook}
-              className="w-10 shrink-0 mt-px mr-px"
+              className="w-10 shrink-0 mt-px mr-px social-icon"
               id="Facebook"
             />
             <img
               src={Apple}
-              className="w-10 shrink-0"
+              className="w-10 shrink-0 social-icon"
               id="Apple"
             />
             <img
               src={Google}
-              className="w-10 shrink-0"
+              className="w-10 shrink-0 social-icon"
               id="Google"
             />
           </div>
--- codey-site/src/components/Signup/index.js
+++ codey-site/src/components/Signup/index.js
@@ -16,107 +16,105 @@ import Google from  '../../assets/google.png';
 
 const Signup = () => {
   return (
-<div class="overflow-hidden bg-white flex flex-col justify-end gap-4 w-full items-end pt-2 px-6" id="signupContainer" >
-<div className="relative flex flex-col w-40 items-start">
-        
+<div class="overflow-hidden bg-white flex flex-row w-full pt-2 px-4" id="signupContainer" >
+    <div className="logo-container">
         <img
           src={Logo}
-          className="relative w-16"
+          className="logo"
         />
         <img
           src={Codey}
-          className="w-24 h-8 absolute top-5 left-12"
+          className="logo logo-second"
           id="Codey"
         />
-        
       </div>
-        <div class="self-stretch flex flex-row gap-16 items-start mx-40" >
-                <div class="self-end relative flex flex-row mt-10 gap-24 w-3/5 items-start" >
-                <div class="text-5xl font-['Poppins'] font-semibold absolute top-0 left-0 h-20 w-64" id="SignUpTo" >
-                Sign Up to
-{" "}
-            </div><div class="text-4xl font-['Poppins'] font-medium absolute top-16 left-2 h-12 w-[427px]" >
-                Become A Youth Coder !
-            </div><div class="relative flex flex-col mt-48 gap-1 items-start" >
-                <div class="font-['Poppins']" >
-                If you already have an account
-{" "}
-            </div><div class="font-['Poppins']" id="YouCanLoginHere1" >
-                You can
-{"   "}     <div class="font-semibold text-[#4d47c3] contents" id="YouCanLoginHere" >
-                <Link to="/signin">Login here</Link>
-            </div>
-            </div>
-            </div><img src={Saly} class="self-end relative mt-16" id="Saly"  />
-            </div>
+      <div className="flex flex-row gap-20 items-center mx-60" id="signup">
+        <div className="flex flex-row mt-60 gap-5 w-3/5 items-center">
+          <div className="flex flex-col gap-12 w-1/2">
             
-            <div class="flex flex-col gap-4 h-[547px] items-start" >
-            <div class="text-3xl font-['Poppins'] font-medium mb-2 mr-[406px]" >
-                Sign Up
+              <div className="text-8xl font-['Poppins'] font-bold"
+                id="Header"
+              >
+                <h1>Sign Up to {" "}</h1>
+              </div>
+              <div className="text-4xl font-['Poppins'] font-medium">
+                Become A Youth Coder
+              </div>
+              <br />
+            
+              <div className="font-['Poppins']">
+                If you already have an account
+              <div className="font-['Poppins']" id="YouCanLoginrHere1">
+                You can{" "}
+                 <Link to="/signup" className="font-semibold custom-link" id="YouCanLoginHere">Login here</Link>
+                
+              </div>
             </div>
+          </div>
+        </div>
+        <div>
+        <img src={Saly} class="self-end relative mt-16" id="Saly"  />
+        </div>
+            
+            <div class="flex flex-col gap-4 h-[543px] items-cente" id="signup-form">
+              <div class="text-3xl font-['Poppins'] font-medium mb-3" >
+                Sign Up
+              </div>
 
-            <div class="bg-[rgba(31,_216,_145,_0.26)] flex flex-row justify-between mb-px w-[369px] h-16 shrink-0 items-center px-8 rounded-lg" >
+              <div class="bg-[rgba(31,_216,_145,_0.26)] flex flex-row justify-between mb-px w-[369px] h-16 shrink-0 items-center px-8 rounded-lg" >
                 
                 <input type="text" class="Text-sm" placeholder="Enter email " />
-            </div>   
-            <div class="bg-[rgba(31,_216,_145,_0.26)] flex flex-col justify-center pl-6 w-[369px] h-16 shrink-0 items-start rounded-lg" >
+              </div>   
+              <div class="bg-[rgba(31,_216,_145,_0.26)] flex flex-col justify-center pl-6 w-[369px] h-16 shrink-0 items-start rounded-lg" >
                 <div class="text-sm font-['Poppins']" >
                 <input type="text" class="Text-sm" placeholder="Create Username" />
 
-            </div>
-            </div><div class="bg-[rgba(31,_216,_145,_0.26)] flex flex-col justify-center pl-6 w-[369px] h-16 shrink-0 items-start rounded-lg" >
-                <div class="font-['Poppins']" >
-                <input type="number" class="Text-sm" placeholder="Contact number" />
+                </div>
+                </div>
+                <div class="bg-[rgba(31,_216,_145,_0.26)] flex flex-col justify-center pl-6 w-[369px] h-16 shrink-0 items-start rounded-lg" >
+                  <div class="font-['Poppins']" >
+                    <input type="number" class="Text-sm" placeholder="Contact number" />
 
-            </div>
-            </div>
+                  </div>
+                </div>
 
-            <div className="bg-[rgba(31,_216,_145,_0.26)] flex flex-row justify-between w-[369px] h-16 shrink-0 items-center px-8 rounded-lg">
-                <input type="password" class="Text-sm" placeholder="Password" />
-            <img
-              src={Invisible}
-              className="w-4 shrink-0"
-              id="Invisible"
-            />
-           </div>
-           <div className="bg-[rgba(31,_216,_145,_0.26)] flex flex-row justify-between w-[369px] h-16 shrink-0 items-center px-8 rounded-lg">
-                <input type="password" class="Text-sm" placeholder="Confirm Password" />
-            <img
-              src={Invisible}
-              className="w-4 shrink-0"
-              id="Invisible"
-            />
-           </div>
-            <div class="shadow-[0px_4px_61px_0px_rgba(77,_71,_195,_0.4)] bg-[#1fd891] flex flex-col justify-center mb-8 w-[369px] h-16 shrink-0 items-center rounded-lg" >
+              <div className="bg-[rgba(31,_216,_145,_0.26)] flex flex-row justify-between w-[369px] h-16 shrink-0 items-center px-8 rounded-lg">
+                  <input type="password" class="Text-sm" placeholder="Password" />
+                 
+              </div>
+              <div className="bg-[rgba(31,_216,_145,_0.26)] flex flex-row justify-between w-[369px] h-16 shrink-0 items-center px-8 rounded-lg">
+                  <input type="password" class="Text-sm" placeholder="Confirm Password" />
+              </div>
+              <div class="shadow-[0px_4px_61px_0px_rgba(77,_71,_195,_0.4)] bg-[#1fd891] flex flex-col justify-center mb-8 w-[369px] h-16 shrink-0 items-center rounded-lg" >
                 <div class="font-['Poppins'] font-medium text-white" >
                 <button>Register</button>
-            </div>
-            </div><div class="font-['Poppins'] font-medium text-[#b5b5b5] self-center mb-6" >
+              </div>
+              </div>
+              <div class="font-['Poppins'] font-medium text-[#b5b5b5] self-center mb-6" >
                 or continue with
             
-        
-            </div>
+              </div>
 
-            <div className="flex flex-row gap-5 w-40 items-start">
-            <img
-              src={Facebook}
-              className="w-10 shrink-0 mt-px mr-px"
-              id="Facebook"
-            />
-            <img
-              src={Apple}
-              className="w-10 shrink-0"
-              id="Apple"
-            />
-            <img
-              src={Google}
-              className="w-10 shrink-0"
-              id="Google"
-            />
-          </div>
+              <div className="flex flex-row gap-5 w-40 items-start" id="social-icons">
+                <img
+                  src={Facebook}
+                  className="w-10 shrink-0 mt-px mr-px social-icon"
+                  id="Facebook"
+                />
+                <img
+                  src={Apple}
+                  className="w-10 shrink-0 social-icon"
+                  id="Apple"
+                />
+                <img
+                  src={Google}
+                  className="w-10 shrink-0 social-icon"
+                  id="Google"
+                />
+              </div>
+           </div>
          </div>
-        </div>
-    </div>
+     </div>
 
   )
 }
